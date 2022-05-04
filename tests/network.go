8dc02b1b76c71e4269e4056c7cb2c22b41291977
Ram Lavi <ralavi@redhat.com>
2022-05-04 11:15:18 +0300

    test/network.go, Fix test for new init-interfaces
    Due to change in init-interfaces script to keep the connection up with
    specific ipv4 enabled/disabled, the tests needs to change accordingly.
    Also, since currently there is need to test upgrade scenario, we can
    simpify the tests.
    
    Merged the two current tests
    'rhcos.network.init-interfaces-pre-configured' and
    'rhcos.network.init-interfaces-not-configured' to
    'rhcos.network.init-interfaces-test'.
    The new test checks 2 scenarios:
    - new setup scenario on fresh node, where there are not connections created.
    - setup after node reboot, that makes sure the
    interfaces don't change after reboot (when MCO reruns the
    init-interfaces script).
    The test is also changed to check that the IPs of the interfaces are in
    the correct state.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- tests/network.go
+++ tests/network.go
@@ -17,10 +17,10 @@ package misc
 import (
 	"encoding/base64"
 	"fmt"
-	"time"
 	"net"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/coreos/mantle/kola/cluster"
 	"github.com/coreos/mantle/kola/register"
@@ -39,20 +39,12 @@ func init() {
 		Timeout:     20 * time.Minute,
 	})
 	register.RegisterTest(&register.Test{
-		Run:         InitInterfacesPreConfigured,
+		Run:         InitInterfacesTest,
 		ClusterSize: 0,
-		Name:        "rhcos.network.init-interfaces-pre-configured",
+		Name:        "rhcos.network.init-interfaces-test",
 		Distros:     []string{"rhcos"},
 		Platforms:   []string{"qemu-unpriv"},
-		Timeout:     20 * time.Minute,
-	})
-	register.RegisterTest(&register.Test{
-		Run:         InitInterfacesNotConfigured,
-		ClusterSize: 0,
-		Name:        "rhcos.network.init-interfaces-not-configured",
-		Distros:     []string{"rhcos"},
-		Platforms:   []string{"qemu-unpriv"},
-		Timeout:     20 * time.Minute,
+		Timeout:     40 * time.Minute,
 	})
 }
 
@@ -68,57 +60,74 @@ func NetworkSecondaryNics(c cluster.TestCluster) {
 	checkExpectedMACs(c, m, expectedMacsList)
 }
 
-// InitInterfacesPreConfigured checks the scenario where the connections are already pre-configured so the script does
-// not configured anything
-// The test checks init-interfaces script on ignition context since MCO is not available on this test infra
-func InitInterfacesPreConfigured(c cluster.TestCluster) {
-	primaryMac := "52:55:00:d1:56:00"
-	secondaryMac := "52:55:00:d1:56:01"
-
-	setupWithInterfacesTest(c, primaryMac, secondaryMac)
-
-	m := c.Machines()[0]
-	macConnectionMap, err := getMacConnectionMap(c, m)
-	if err != nil {
-		c.Fatalf(fmt.Sprintf("failed to get macConnectionMap: %v", err))
-	}
-	err = checkExpectedInterfacesStatus(c, m, macConnectionMap, []string{primaryMac}, []string{secondaryMac})
-	if err != nil {
-		c.Fatalf(fmt.Sprintf("interfaces are not in expected status when connections are pre-configured: %v", err))
-	}
-}
-
-// InitInterfacesNotConfigured checks the scenario where the connections don't exist the script creates them
-// The test checks init-interfaces script on ignition context since MCO is not available on this test infra
-func InitInterfacesNotConfigured(c cluster.TestCluster) {
+// InitInterfacesTest checks init-interfaces script in both fresh setup and Node reboot
+// In this test MCO is checked as part of ignition context since MCO is not available on this test infra
+func InitInterfacesTest(c cluster.TestCluster) {
 	primaryMac := "52:55:00:d1:56:00"
 	secondaryMac := "52:55:00:d1:56:01"
 
 	setupWithInterfacesTest(c, primaryMac, secondaryMac)
 	m := c.Machines()[0]
-	simulateNoConnections(c, m, []string{primaryMac, secondaryMac})
-
-	macConnectionMap, err := getMacConnectionMap(c, m)
+	err := simulateNewInstallation(c, m, []string{primaryMac, secondaryMac})
 	if err != nil {
-		c.Fatalf(fmt.Sprintf("failed to get macConnectionMap: %v", err))
+		c.Fatalf("failed to simulate new setup with no connections: %v", err)
 	}
-	err = checkExpectedInterfacesStatus(c, m, macConnectionMap, []string{primaryMac}, []string{secondaryMac})
+	err = checkExpectedInterfaces(c, m, primaryMac, secondaryMac)
 	if err != nil {
-		c.Fatalf(fmt.Sprintf("interfaces are not in expected status when connections do not exist: %v", err))
+		c.Fatalf("failed when no connections are configured: %v", err)
+	}
+
+	err = simulateNodeReboot(c, m)
+	if err != nil {
+		c.Fatalf("failed to simulate Node reboot: %v", err)
+	}
+	err = checkExpectedInterfaces(c, m, primaryMac, secondaryMac)
+	if err != nil {
+		c.Fatalf("failed when after setup reboot: %v", err)
 	}
 }
 
-func simulateNoConnections(c cluster.TestCluster, m platform.Machine, macConnectionsToDelete []string) {
+func checkExpectedInterfaces(c cluster.TestCluster, m platform.Machine, primaryMac, secondaryMac string) error {
 	macConnectionMap, err := getMacConnectionMap(c, m)
 	if err != nil {
-		c.Fatalf(fmt.Sprintf("failed to get macConnectionMap: %v", err))
+		return fmt.Errorf("failed to get macConnectionMap: %v", err)
 	}
+	err = checkExpectedInterfacesStatus(c, m, macConnectionMap, []string{primaryMac, secondaryMac}, []string{})
+	if err != nil {
+		return fmt.Errorf("interfaces are not in expected status when connections do not exist: %v", err)
+	}
+	err = checkExpectedInterfacesIPAddress(c, m, macConnectionMap, []string{primaryMac}, []string{secondaryMac})
+	if err != nil {
+		return fmt.Errorf("interfaces are not in expected status when connections do not exist: %v", err)
+	}
+	return nil
+}
 
+func simulateNewInstallation(c cluster.TestCluster, m platform.Machine, macConnectionsToDelete []string) error {
+	macConnectionMap, err := getMacConnectionMap(c, m)
+	if err != nil {
+		return fmt.Errorf("failed to get macConnectionMap: %v", err)
+	}
+	macInterfaceMap, err := getMacInterfaceMap(c, m)
+	if err != nil {
+		return fmt.Errorf("failed to get macInterfaceMap: %v", err)
+	}
+	flushInterfaceIpByMac(c, m, macInterfaceMap, macConnectionsToDelete)
 	removeConnectionsByMac(c, m, macConnectionMap, macConnectionsToDelete)
+
 	err = m.Reboot()
 	if err != nil {
-		c.Fatalf("failed to reboot the machine: %v", err)
+		return fmt.Errorf("failed to reboot the machine: %v", err)
 	}
+	return nil
+}
+
+func simulateNodeReboot(c cluster.TestCluster, m platform.Machine) error {
+	err := m.Reboot()
+	if err != nil {
+		return fmt.Errorf("failed to reboot the machine: %v", err)
+	}
+	return nil
 }
 
 func setupWithInterfacesTest(c cluster.TestCluster, primaryMac, secondaryMac string) {
@@ -215,10 +224,17 @@ func getCaptureMacsScript() (string, error) {
 	return string(data), nil
 }
 
-func removeConnectionsByMac(c cluster.TestCluster, m platform.Machine, macConnectionMap map[string]string, macsList[]string) {
+func removeConnectionsByMac(c cluster.TestCluster, m platform.Machine, macConnectionMap map[string]string, macsList []string) {
 	for _, mac := range macsList {
 		connectionToDelete := macConnectionMap[mac]
 		c.MustSSH(m, fmt.Sprintf("sudo nmcli con del '%s'", connectionToDelete))
+	}
+}
+
+func flushInterfaceIpByMac(c cluster.TestCluster, m platform.Machine, macInterfaceMap map[string]string, macsList []string) {
+	for _, mac := range macsList {
+		InterfaceToFlush := macInterfaceMap[mac]
+		c.MustSSH(m, fmt.Sprintf("sudo ip addr flush dev '%s'", InterfaceToFlush))
 	}
 }
 
@@ -255,6 +271,39 @@ func getConnectionStatus(c cluster.TestCluster, m platform.Machine, connectionNa
 	return string(c.MustSSH(m, fmt.Sprintf("nmcli -f GENERAL.STATE con show '%s' | awk '{print $2}'", connectionName)))
 }
 
+func checkExpectedInterfacesIPAddress(c cluster.TestCluster, m platform.Machine, macConnectionMap map[string]string, expectedIpEnabledInterfaces, expectedIpDisabledInterfaces []string) error {
+	failedConnections := []string{}
+	for _, ifaceMac := range expectedIpEnabledInterfaces {
+		connectionName := macConnectionMap[ifaceMac]
+		if !isConnectionIpv4Enabled(c, m, connectionName) {
+			failedConnections = append(failedConnections, fmt.Sprintf("expected connection %s to have an IPv4 Address", connectionName))
+		}
+	}
+
+	for _, ifaceMac := range expectedIpDisabledInterfaces {
+		connectionName := macConnectionMap[ifaceMac]
+		if isConnectionIpv4Enabled(c, m, connectionName) {
+			failedConnections = append(failedConnections, fmt.Sprintf("expected connection %s to not have an IPv4 Address", connectionName))
+		}
+	}
+
+	if len(failedConnections) != 0 {
+		return fmt.Errorf(strings.Join(failedConnections, ","))
+	}
+	return nil
+}
+
+func isConnectionIpv4Enabled(c cluster.TestCluster, m platform.Machine, connectionName string) bool {
+	if getConnectionIpv4Addresses(c, m, connectionName) == "" {
+		return false
+	}
+	return true
+}
+
+func getConnectionIpv4Addresses(c cluster.TestCluster, m platform.Machine, connectionName string) string {
+	return string(c.MustSSH(m, fmt.Sprintf("nmcli -g IP4.ADDRESS con show '%s'", connectionName)))
+}
+
 func checkExpectedMACs(c cluster.TestCluster, m platform.Machine, expectedMacsList []string) {
 	macConnectionMap, err := getMacConnectionMap(c, m)
 	if err != nil {
@@ -280,8 +329,8 @@ func getConnectionDeviceMap(c cluster.TestCluster, m platform.Machine, connectio
 
 func getConnectionsList(c cluster.TestCluster, m platform.Machine) []string {
 	output := string(c.MustSSH(m, "nmcli -t -f NAME con show"))
-	interfaceNames := strings.Split(output, "\n")
-	return interfaceNames
+	connectionNames := strings.Split(output, "\n")
+	return connectionNames
 }
 
 func getMacConnectionMap(c cluster.TestCluster, m platform.Machine) (map[string]string, error) {
@@ -291,15 +340,33 @@ func getMacConnectionMap(c cluster.TestCluster, m platform.Machine) (map[string]
 		return nil, fmt.Errorf("failed to get connectionDeviceMap: %v", err)
 	}
 
-	MacInterfaceMap := map[string]string{}
+	macConnectionMap := map[string]string{}
 	for _, connection := range connectionNamesList {
 		interfaceMACAddress, err := getDeviceMAC(c, m, connectionDeviceMap[connection])
 		if err != nil {
 			return nil, fmt.Errorf("failed to fetch connection %s MAC Address: %v", connection, err)
 		}
-		MacInterfaceMap[interfaceMACAddress] = connection
+		macConnectionMap[interfaceMACAddress] = connection
 	}
-	return MacInterfaceMap, nil
+	return macConnectionMap, nil
+}
+
+func getMacInterfaceMap(c cluster.TestCluster, m platform.Machine) (map[string]string, error) {
+	connectionNamesList := getConnectionsList(c, m)
+	connectionDeviceMap, err := getConnectionDeviceMap(c, m, connectionNamesList)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get connectionDeviceMap: %v", err)
+	}
+
+	macInterfaceMap := map[string]string{}
+	for _, connection := range connectionNamesList {
+		interfaceMACAddress, err := getDeviceMAC(c, m, connectionDeviceMap[connection])
+		if err != nil {
+			return nil, fmt.Errorf("failed to fetch connection %s MAC Address: %v", connection, err)
+		}
+		macInterfaceMap[interfaceMACAddress] = connectionDeviceMap[connection]
+	}
+	return macInterfaceMap, nil
 }
 
 func getDeviceMAC(c cluster.TestCluster, m platform.Machine, deviceName string) (string, error) {
